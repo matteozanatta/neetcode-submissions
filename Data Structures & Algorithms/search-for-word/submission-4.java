dd68ec33d298793e5d97c0d44ebbba4a79e0bc90
Matteo Zanatta <36624679+matteozanatta@users.noreply.github.com>
2026-06-27 15:03:01 +0200

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if(board == null || board.length == 0) return false;
+        if(word == null || word.trim().isEmpty()) return true;
+
+        boolean res = false;
+
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+                if(board[i][j] == word.charAt(0)){
+                    res = dfs(i, j, board, 0, word);
+                    if(res) return res;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(int row, int col, char[][] board, int index, String word){
+        if(index == word.length()){
+            return true;
+        }
+
+        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length){
+            return false;
+        }
+
+        if(word.charAt(index) != board[row][col]){
+            return false;
+        }
+
+        board[row][col] = '#';
+
+        boolean down = dfs(row+1, col, board, index + 1, word);
+        if(down) return true;
+        boolean up = dfs(row-1, col, board, index + 1, word);
+        if(up) return true;
+        boolean right = dfs(row, col+1, board, index + 1, word);
+        if(right) return true;
+        boolean left = dfs(row, col-1, board, index + 1, word);
+        if(left) return true;
+
+        board[row][col] = word.charAt(index);
+
+        return false;
+    }
+}
